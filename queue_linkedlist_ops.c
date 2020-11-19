1aa8475b7f6fcd8a744b316946045b0d97096b8b
Manas Pant <manas.pant@pasarpolis.com>
2020-11-19 20:48:56 +0530

    working version of queue operations using linked list

--- queue_linkedlist_ops.c
+++ queue_linkedlist_ops.c
@@ -7,17 +7,61 @@ struct Node
     struct Node *next;
 };
 
-//manas
+struct Node *front, *rear = NULL;
 
-void enqueue(struct Node *r, int x)
+void linklistTraversal()
 {
-    if(!isFull(q))
+    int i = 1;
+    struct Node *a = front;
+    while(a != NULL)
     {
-        struct Node *q = (struct Node *)malloc(sizeof(struct Node));
-        q->data = x;
-        q->next = r;
-         
+        printf("\nelement %d in list is: %d", i, a->data);
+        a = a->next;
+        i++;
+    }
+}
 
+int isFull()
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    if(p == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isEmpty()
+{
+    if(front == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void enqueue(int x)
+{
+    if(!isFull())
+    {
+        struct Node *inserted = (struct Node *)malloc(sizeof(struct Node));
+        inserted->data = x;
+        inserted->next = NULL;
+        if(front == NULL)
+        {
+            front = rear = inserted;
+        }
+        else
+        {
+            rear->next = inserted;
+            rear = inserted;
+        }
     }
     else
     {
@@ -25,22 +69,39 @@ void enqueue(struct Node *r, int x)
     }
 }
 
+int dequeue()
+{
+    int val = -1;
+    if(!isEmpty())
+    {
+        struct Node *ptr = front;
+        val = front->data;
+        front = front->next;
+        free(ptr);
+    }
+    else
+    {
+        printf("\nqueue is empty, cannot delete any element");
+    }
+    return val;
+}
+
 int main()
 {
-    struct Node *queue = (struct Node *)malloc(sizeof(struct Node));
-    queue = NULL;
 
-    enqueue(queue, 21);
-    enqueue(queue, 38);
-    enqueue(queue, 20);
-    enqueue(queue, 47);
-    enqueue(queue, 91);
-    enqueue(queue, 76);
-    enqueue(queue, 11);
+    enqueue(21);
+    enqueue(38);
+    enqueue(20);
+    enqueue(47);
+    enqueue(91);
+    enqueue(76);
+    enqueue(11);
 
-    printf("\nremoved element is: %d", dequeue(queue));
-    printf("\nremoved element is: %d", dequeue(queue));
-    printf("\nremoved element is: %d", dequeue(queue));
+    linklistTraversal();
+
+    printf("\nremoved element is: %d", dequeue());
+    printf("\nremoved element is: %d", dequeue());
+    printf("\nremoved element is: %d", dequeue());
 
     return 0;
 }
